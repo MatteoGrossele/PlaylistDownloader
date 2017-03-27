f65f71a8d1bae8daa41d8a6567af896fbe8c85f2
MatteoGrossele <matteogross@live.it>
2017-03-27 16:53:48 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PlaylistDownloader
 Program to download atomatically song from youtube starting from json db
+
+#Install
+Requires youtube_dl (which requires ffmpeg) and eyed3
+
+sudo pip install eyeD3
+
+sudo pip install youtube_dl
+
+sudo apt-get install ffmpeg
---
+++ music_downloader.py
@@ -0,0 +1,60 @@
+from __future__ import unicode_literals
+import json
+
+import youtube_dl
+import eyed3
+import shutil
+import os
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'downloaded.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+SEPARATOR = "-"
+DIRECTORY = "playlist"
+IMAGE="wall.jpg"
+
+playlist = None
+json_data = open('playlist.json')
+playlist = json.load(json_data)
+
+# Check if playlist directory is present , if not create it
+if not os.path.exists(DIRECTORY):
+	os.makedirs(DIRECTORY)
+
+for song in playlist['playlist']['song'] :
+
+	#Create filename
+	if song['subdir'] :
+		filename = song['subdir'] + "/" + song['artist'] + SEPARATOR + song['title'] +".mp3"
+	else :
+		filename = song['artist'] + SEPARATOR + song['title'] +".mp3"
+
+	#Skip song if already downloaded
+	if os.path.isfile(DIRECTORY+"/"+filename) :
+		continue
+
+	#Download song from youtube
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		print ydl.download([song['ytlink']])
+
+	#Set ID3 tags for the downloaded file
+	audiofile = eyed3.load("downloaded.mp3")
+	
+	audiofile.tag.artist = song['artist']
+	audiofile.tag.title = song['title']
+	imagedata = open(IMAGE,"rb").read()
+	audiofile.tag.images.set(3,imagedata,"image/jpeg",u"Album title")
+	#audiofile.tag.images = ""
+	#audiofile.tag.Date = song['year']
+
+	audiofile.tag.save()
+
+	#Move the file inside the folder
+	
+	shutil.move("downloaded.mp3",DIRECTORY+"/"+filename)
