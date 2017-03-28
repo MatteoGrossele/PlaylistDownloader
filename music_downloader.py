0e909b183cb30d1974d5c4400ad3aca5a6b6d4de
matteogabburo <matteogabburo@gmail.com>
2017-03-28 13:46:33 +0200

    pep8 formatted

--- music_downloader.py
+++ music_downloader.py
@@ -1,3 +1,5 @@
+#!/bin/python2.7
+
 from __future__ import unicode_literals
 import json
 
@@ -26,37 +28,38 @@ playlist = json.load(json_data)
 
 # Check if playlist directory is present , if not create it
 if not os.path.exists(DIRECTORY):
-	os.makedirs(DIRECTORY)
+    os.makedirs(DIRECTORY)
 
-for song in playlist['playlist']['song'] :
+for song in playlist['playlist']['song']:
 
-	#Create filename
-	if song['subdir'] :
-		filename = song['subdir'] + "/" + song['artist'] + SEPARATOR + song['title'] +".mp3"
-	else :
-		filename = song['artist'] + SEPARATOR + song['title'] +".mp3"
+    #Create filename
+    if song['subdir']:
+        filename = song['subdir'] + "/" + song['artist'] + SEPARATOR + \
+            song['title'] + ".mp3"
+    else:
+        filename = song['artist'] + SEPARATOR + song['title'] + ".mp3"
 
-	#Skip song if already downloaded
-	if os.path.isfile(DIRECTORY+"/"+filename) :
-		continue
+    #Skip song if already downloaded
+    if os.path.isfile(DIRECTORY + "/" + filename):
+        continue
 
-	#Download song from youtube
-	print "Downloading ->" + filename + "\n"
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		ydl.download([song['ytlink']])
+    #Download song from youtube
+    print("Downloading ->" + filename + "\n")
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([song['ytlink']])
 
-	#Set ID3 tags for the downloaded file
-	audiofile = eyed3.load("downloaded.mp3")
-	
-	audiofile.tag.artist = song['artist']
-	audiofile.tag.title = song['title']
-	imagedata = open(IMAGE,"rb").read()
-	audiofile.tag.images.set(3,imagedata,"image/jpeg",u"Album title")
-	#audiofile.tag.images = ""
-	#audiofile.tag.Date = song['year']
+    #Set ID3 tags for the downloaded file
+    audiofile = eyed3.load("downloaded.mp3")
 
-	audiofile.tag.save()
+    audiofile.tag.artist = song['artist']
+    audiofile.tag.title = song['title']
+    imagedata = open(IMAGE, "rb").read()
+    audiofile.tag.images.set(3, imagedata, "image/jpeg", "Album title")
+    #audiofile.tag.images = ""
+    #audiofile.tag.Date = song['year']
 
-	#Move the file inside the folder
-	
-	shutil.move("downloaded.mp3",DIRECTORY+"/"+filename)
+    audiofile.tag.save()
+
+    #Move the file inside the folder
+
+    shutil.move("downloaded.mp3", DIRECTORY + "/" + filename)
